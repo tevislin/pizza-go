eef8d5de39b0ff050a3bd6f85fe2c867dcdb01c1
tevislin <tevislin18@gmail.com>
2021-03-07 12:19:22 +0300

    Added extra functions to the select bar

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(event){
+    var pizzaType=document.querySelector("#pizzaselect").val();
+    
+    function wordChange(){
+        if (pizzaType="Tomato"){
+            alert("Tomato")
+        }
+    }
+
+})
