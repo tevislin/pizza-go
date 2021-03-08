02d992ba272cbb3dee7592a3b048719a0e47c73b
tevislin <tevislin18@gmail.com>
2021-03-08 17:58:29 +0300

    Added all the functions to the pizza order

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@ $("document").ready(function(){
 
   $("#submitbutton").click(function(){
 
-   
+   $("#order2").show();
 
     var pizzaType =parseFloat(document.getElementById("type").value);
     var pizzaSize = parseFloat(document.querySelector("#size").value);
@@ -24,6 +24,9 @@ $("document").ready(function(){
     var potato = document.querySelector("#potato").checked;
     var olives= document.querySelector("#olives").checked;
     var amount= parseFloat(document.querySelector("#amount").value);
+    var inputStreet = document.querySelector("#street").value;
+    var inputCity = document.querySelector("#city").value;
+    var inputCountry = document.querySelector("#country").value;
     
         pizzaWord = document.getElementById("type").value;
      
@@ -70,6 +73,17 @@ var topingChanger= function(){
     return sausage+olives+potato
 
 }
+var pizzaSizeChanger= function(){
+    if(pizzaSize == 0){
+        return "small"
+    }
+    else if(pizzaSize == 5){
+        return "medium"
+    }
+    else if(pizzaSize == 10){
+        return large
+    }
+}
 var pizzaTypeChanger= function(){
     
     if(pizzaType == 10){
@@ -96,11 +110,31 @@ var totalPrice= $ +AddAll *amount;
 
 document.querySelector("#price").innerHTML="Total price is $"+AddAll * amount;
 
-document.querySelector("#moredetails").innerHTML="Type of pizza is " + pizzaTypeChanger();
+document.querySelector("#moredetails").innerHTML="Pizza type is " + pizzaTypeChanger()+" size "+pizzaSizeChanger();
   
 document.querySelector("#crustinfo").innerHTML="Type of crust is " + pizzaCrustChanger();
 
-document.querySelector("#topinfo").innerHTML="The toppings are " + topingChanger();
+document.querySelector("#yourorder").innerHTML="Your order" ;
+
+document.querySelector("#topinfo").innerHTML="The toppings are checked on the left" ;
+
+document.querySelector("#addresses").innerHTML= "Shipping adress:"+inputStreet + ", " + inputCity + ", "+ inputCountry ;
+
+document.querySelector("#amountorder").innerHTML ="You ordered "+ amount +" pizzas";
+
+   $("#confirm").click(function(){
+    var amount= parseFloat(document.querySelector("#amount").value);
+    var inputStreet = document.querySelector("#street").value= "";
+    var inputCity = document.querySelector("#city").value="";
+    var inputCountry = document.querySelector("#country").value="";
+    var sausage = document.querySelector("#sausage").checked;
+    var potato = document.querySelector("#potato").checked;
+    var olives= document.querySelector("#olives").checked;
+    $("#order2").hide();
+
+    alert("Your order is confirmed!!!");
+    
+   })
+})
 })
 
-})
