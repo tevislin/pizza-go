b567ffbe8622d815262555386e614f71cf197aa9
tevislin <tevislin18@gmail.com>
2021-03-08 06:40:19 +0300

    Added select options

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
-$(document).ready(function(){
-    $("#orderFirst").click(function(){
-        $(".order1").toggle();
-    $(".menu").toggle();
-    
-    })
+$("document").ready(function(){
 
-    $(".card2").click(function(){
-        $(".icon2").toggle();
-        $(".c-text2").toggle();
-    })
+    function Pizza(type, size, crust, top){
+        this.type= type;
+        this.size= size;
+        this.crust = crust;
+        this.top = top;
+    }
+
+    var inputType= document.querySelector("#type").value;
+    
 })
